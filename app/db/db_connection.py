405cc63b13efc531c01409f8e28d9fff132758c9
Alexander <sasha.14999933@gmail.com>
2025-03-04 20:10:57 +0500

    alembic init and db connection

---
+++ app/db/db_connection.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from app.core import settings
+
+
+DATABASE_URL = f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
+
+engine = create_async_engine(DATABASE_URL)
